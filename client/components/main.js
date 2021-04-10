8e4c726e7d61cbceb4f7ad4d8ebc6ccf11da404e
Max Smagin <Max@MacBook-Air-MS.local>
2021-04-10 20:38:31 +0300

    task2

---
+++ client/components/main.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Head from './head'
+
+const Main = () => {
+  return (
+    <div>
+      <Head title="Main" />
+      <div className="flex items-center justify-center h-screen">
+        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+          <div className="flex justify-center border rounded font-semibold mb-2 p-2" id="title">
+            Main
+          </div>
+          <div className="flex flex-col items-center">
+            <Link to="/dashboard/profile/ec465285-1fff-4f1e-8142-57f4bb2a42b7">Go To Profile</Link>
+            <Link to="/dashboard">Go To Root</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Main.propTypes = {}
+
+export default Main
